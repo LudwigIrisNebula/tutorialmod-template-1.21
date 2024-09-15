34e562882d7d13b438db75167c67816c50fcb898
15460IrisNebula1300 <154600985@qq.com>
2024-09-15 20:40:37 +0800

    Added a new item called "Super White Diamond"fcb1ff913705d774be7b7b96218878c581c8b614.

---
+++ src/main/java/com/besson/tutorialmod/item/Moditems.java
@@ -0,0 +1,49 @@
+package com.besson.tutorialmod.item;
+
+import com.besson.tutorialmod.TutorialMod;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
+
+public class Moditems {
+    public static final Item SUPER_WHITE_DIAMOND = registerItems("super_white_diamond", new Item(new Item.Settings()));
+//    一个简单的物品，超级白钻。
+    private static Item registerItems(String id,Item item){
+//        return Registry.register(Registries.ITEM, RegistryKey.of(Registries.ITEM.getKey(), Identifier.of(TutorialMod.MOD_ID,id)), item);
+        return Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID,id), item);
+    }
+    public static void registerModItems(){
+        TutorialMod.LOGGER.info("Registering Items");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
